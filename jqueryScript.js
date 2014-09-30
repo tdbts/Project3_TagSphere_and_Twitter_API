e88f7a922732ceb4323d0cfc5be81e6c86a1b157
tdbts <VinnySanchez87@gmail.com>
2014-09-30 17:04:24 -0400

    Damn that's weird...so cloud.js doesn't work when you declare html in your index document...gotta figure out how I can fix this

--- jqueryScript.js
+++ jqueryScript.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	var userCloudTags = [];
+	// var userCloudTags = [];
 
 	$('#header').fadeOut(700).fadeIn(700);
 
@@ -28,30 +28,40 @@ $(document).ready(function() {
 		}
 	});
 
-	function init() {
+	// function init() {
+	// 	// var w = document.body.clientWidth, h = document.body.clientHeight;
+	// 	// var clouder = document.getElementById('clouder');
 
-		var w = $('body').width();
-		var h = $('body').height();
-		var clouder = $('#clouder');
+	// 	// clouder.style.border = "1px solid black";
+	// 	// clouder.style.width = w * 2 / 3;
+	// 	// clouder.style.height = h * 2 / 3;
+	// 	// clouder.style.position = "absolute";
+	// 	// clouder.style.left = w / 6;
+	// 	// clouder.style.top = h / 6;
 
-		clouder.css('border', '1px solid black');
-		clouder.width(w * 2 / 3);
-		clouder.height(h * 2 / 3);
-		clouder.css('position', 'absolute');
-		clouder.css('left', w / 6);
-		clouder.css('top', h / 6);
 		
-		window.clouder = new Clouder({
-			container: clouder,
-			// tags: userCloudTags
-			tags: [{text: "testing", id: "0", weight: 0.5}, {text: "muthafucka", id: "1", weight: 0.5}]
-		});
-	}
+	// 	var w = $('body').width();
+	// 	var h = $('body').height();
+	// 	var clouder = $('#clouder');
 
-	$('#drawCloud').click(function() {
+	// 	clouder.css('border', '1px solid black');
+	// 	clouder.width(w * 2 / 3);
+	// 	clouder.height(h * 2 / 3);
+	// 	clouder.css('position', 'absolute');
+	// 	clouder.css('left', w / 6);
+	// 	clouder.css('top', h / 6);
 		
-		init();
-	});
+	// 	window.clouder = new Clouder({
+	// 		container: clouder,
+	// 		// tags: userCloudTags
+	// 		tags: [{text: "testing", id: "0", weight: 0.5}, {text: "muthafucka", id: "1", weight: 0.5}]
+	// 	});
+	// }
+
+	// $('#drawCloud').click(function() {
+		
+	// 	init();
+	// });
 
 });
 
--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+function init() {
+	var w = document.body.clientWidth, h = document.body.clientHeight;
+	var clouder = document.getElementById('clouder');
+
+	clouder.style.border = "1px solid black";
+	clouder.style.width = w * 2 / 3;
+	clouder.style.height = h * 2 / 3;
+	clouder.style.position = "absolute";
+	clouder.style.left = w / 6;
+	clouder.style.top = h / 6;
+
+	window.clouder = new Clouder({
+		container: clouder, 
+		tags: [{text: "please", id: "0", weight: 0.5}, {text: "work", id: "1", weight: 0.5}]
+	});
+}
+
+var userCloudTags = [];
+
 function createObjectForCloud(theText, cloudTags) {
 	var theID;
 
