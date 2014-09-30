07c5ee22b361123b41919146446bbc9ae5ba9af8
tdbts <VinnySanchez87@gmail.com>
2014-09-30 14:29:49 -0400

    Trying to figure out why I get get the tag cloud to render

--- jqueryScript.js
+++ jqueryScript.js
@@ -3,53 +3,55 @@ $(document).ready(function() {
 
 	$('#header').fadeOut(700).fadeIn(700);
 
-	// if (typeof(Storage) !== "undefined") {
+	function addTag() {
 
-	// }
-	// else {
-	// 	alert("Your browser does not support local storage.  \n
-	// 		Some parts of this webpage may not work for you.")
-	// }
-	
-	$('#tagButton').click(function() {
-		
 		var userText = $('#customTag').val();
-		console.log("You typed: " + userText);
+		console.log("Adding '" + userText + "' to the cloud.");
 
-		if (typeof(Storage) !== "undefined") {
-			if (userText.length !== 0) {
-				var newUserLittleObject = createObjectForCloud(userText, userCloudTags);
-				console.log("newUserLittleObject is: ");
-				console.log(newUserLittleObject);
-				userCloudTags.push(newUserLittleObject);
-				localStorage.setItem("cloudTags", userCloudTags);
-			}
-		}
-		else {
-			// Alternative code here...
-		}
+		var newUserLittleObject = createObjectForCloud(userText, userCloudTags);
+		userCloudTags.push(newUserLittleObject);
+		console.log(userCloudTags);
 
+		$('#customTag').val('');
+	}
+
+	$('#tagButton').click(function() {
+	
+		addTag();
+	});
+
+	$('#customTag').keydown(function(e) {
+		
+		if (e.keyCode == 13) {
+
+			addTag();
+		}
+	});
+
+	function init() {
+
+		var w = document.body.clientWidth;
+		var h = document.body.clientHeight;
+		var clouder = document.getElementById('clouder');
+
+		clouder.style.border = "1px solid black";
+		clouder.style.width = w * 2 / 3;
+		clouder.style.height = h * 2 / 3;
+		clouder.style.position = "absolute";
+		clouder.style.left = w / 6;
+		clouder.style.top = h / 6;
+		
+		window.clouder = new Clouder({
+			container: clouder,
+			tags: userCloudTags
+		});
+	}
+
+	$('#drawCloud').click(function() {
+		
+		init();
 	});
 
 });
 
-// $(document).ready(function() {
 
-// 	// function init() {
-// 	// 	var w = document.body.clientWidth, h = document.body.clientHeight;
-// 	// 	var clouder = document.getElementById('clouder');
-
-// 	// 	clouder.style.border = "1px solid black";
-// 	// 	clouder.style.width = w * 2 / 3;
-// 	// 	clouder.style.height = h * 2 / 3;
-// 	// 	clouder.style.position = "absolute";
-// 	// 	clouder.style.left = w / 6;
-// 	// 	clouder.style.top = h / 6;
-
-// 	$('#tagButton').click(function() {
-// 		alert("You typed: " + $('#customTag').val());
-// 	});
-
-// 	}
-
-// });
