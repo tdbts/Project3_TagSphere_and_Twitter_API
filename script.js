2d0fc85eb03537253adc72e134253ae44cccfaf3
tdbts <VinnySanchez87@gmail.com>
2014-10-13 16:25:28 -0400

    Before I start cleaning up the jQuery code, this is probably a good point to commit and push what I've done so far

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
+// Have to document this (and all of my code) much more thoroughly so after
+// prolonged periods away from this project I can easily remember 
+// how everything works
+
+// Arrays that will hold the tags used to generate the cloud
+// They are outside of any f(x)'s so that they can be accessed by 
+// Other f(x)'s and code in other files -- I know there is a better 
+// way to do this
 var userCloudTags = [];
 var twitterCloudTags = [];
 
-// Have to document this (and all of my code) much better so after
-// prolonged periods away from this project I can easily remember 
-// how everything works
+// As part of its input, the Clouder class accepts an array of 
+// "little objects" of the form: {text: theText, id: theID, weight: theWeight}
+// This f(x) creates a single "little object" for the cloud
 function createObjectForCloud(theText, cloudTags) {
 	var theID;
-	console.log(theText);
 
-	function createID() {
-		var tagLength = cloudTags.length;
+	// Generates an ID for the new object based upon the number of tags already 
+	// in the tag array.  The first ten tags take the numbers (as strings) 0-9, 
+	// the next ten 00, 01, 02...09, the next ten 000, 001, 002...009, and so on. 
+	function createID(arr) {
+		var tagLength = arr.length;
 		var subtractor = 0;
 
 		// Starting from zero, subtract from the number of elements in the 
@@ -39,11 +49,13 @@ function createObjectForCloud(theText, cloudTags) {
 				return firstDigit.toString() + zeroes;
 			}
 		}
+		// Invoke the 'output' f(x)
 		return output(myFirstDigit, myNumberOfZeroes);
 	}
+	// Invoke 'createID' using array of cloud tags
+	theID = createID(cloudTags);
 
-	theID = createID();
-
+	// Hardcoded weight value...may want to change this
 	return {text: theText, id: theID, weight: 0.1};
 }
 
